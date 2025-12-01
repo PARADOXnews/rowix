c978850caf498bc2c8d99af6a948ed6c92fdc18b
PARADOXnews <achirubashvili@gmail.com>
2025-12-02 00:05:57 +0400

    a

--- src/components/Solutions/Solutions.tsx
+++ src/components/Solutions/Solutions.tsx
@@ -10,12 +10,16 @@ export default function Solutions() {
       <section className={styles.container}>
 
          <div className={styles.left}>
-            {/* <div></div> */}
-            <div className={styles.logo}>
-               <h4 className={styles.h4}>Digital Solutions </h4>
-               <Image src="/Solutions/button.png" width={266} height={77} alt="logo" />
+            <div>
+
+               <div className={styles.logo}>
+                  <h4 className={styles.h4}>Digital Solutions </h4>
+                  <Image src="/Solutions/button.png" width={266} height={77} alt="logo" />
+               </div>
+               <h4 className={styles.h4}>That Drive Success</h4>
+               <p>At NexGen, we believe in the transformative power of digital solutions. Our team of experts is dedicated to helping businesses like yours thrive in the fast-paced digital landscape.</p>
             </div>
-            <h4 className={styles.h4}>That Drive Success</h4>
+            <Image src="/Solutions/text.png"  width={1167} height={70} alt="logo" />
          </div>
       </section>
    )
