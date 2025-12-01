1f05884cd645e218f44221e2658069efce26f671
PARADOXnews <achirubashvili@gmail.com>
2025-12-01 21:35:46 +0400

    a

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
 import styles from "./Header.module.scss";
 
 export default function Header() {
 
    return (
       <header className={styles.a}>
-         {}
+
+         <Image src="" width={200} height={300} alt="foto" />
       </header>
    )
 }
