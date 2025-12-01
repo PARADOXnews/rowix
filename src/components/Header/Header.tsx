1b29db4d03ab5319cff41ab5081c3bd47c826364
PARADOXnews <achirubashvili@gmail.com>
2025-12-01 21:58:45 +0400

    a

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,9 @@ import styles from "./Header.module.scss";
 export default function Header() {
 
    return (
-      <header className={styles.a}>
+      <header className={styles.header}>
          header
-         <Image src="/Header/1.png" width={200} height={300} alt="foto" />
+         {/* <Image src="/Header/1.png" width={200} height={300} alt="foto" /> */}
       </header>
    )
 }
---
+++ src/components/Users/Users.tsx
@@ -0,0 +1,11 @@
+import styles from "./Users.module.scss";
+
+
+export default function Users(){
+
+
+
+   return(
+      <section></section>
+   )
+}
