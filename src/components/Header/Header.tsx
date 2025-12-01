0b12b617d689d167e7223b70523a3a976a38fd85
damdebeli1 <notfunny77777@gmail.com>
2025-12-01 22:07:09 +0400

    dasfawqgwqa

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,9 +4,14 @@ import styles from "./Header.module.scss";
 export default function Header() {
 
    return (
+<<<<<<< Updated upstream
       <header className={styles.header}>
          header
          {/* <Image src="/Header/1.png" width={200} height={300} alt="foto" /> */}
+=======
+      <header className={styles.a}>
+         
+>>>>>>> Stashed changes
       </header>
    )
 }
