891bea39eb841174a1057f557d01d814fd6f2939
PARADOXnews <achirubashvili@gmail.com>
2025-12-02 03:09:50 +0400

    az

--- src/components/Solutions/Solutions.tsx
+++ src/components/Solutions/Solutions.tsx
@@ -8,7 +8,7 @@ export default function Solutions() {
 
    return (
 
-      <section className={styles.container}>
+      <section className={`jcb aic ${styles.container}`}>
 
          <div className={`cflex jcb ${styles.left}`}>
             <div className={`cflex ormoci ${styles.text}`}>
@@ -20,11 +20,16 @@ export default function Solutions() {
                <h4 className={styles.h4}>That Drive Success</h4>
                <p>At NexGen, we believe in the transformative power of digital solutions. Our team of experts is dedicated to helping businesses like yours thrive in the fast-paced digital landscape.</p>
             </div>
-            <Image src="/Solutions/text.png"  width={1167} height={70} alt="logo" />
+            <Image src="/Solutions/text.png" width={1167} height={70} alt="logo" />
          </div>
 
-         <div className={styles.right}>
+         <div className={`cflex ${styles.right}`}>
+            <Image src="/Solutions/advance.png" height={465} width={593} alt="logo" />
 
+            <div className={`cflex tormeti ` + styles.state}>
+               <h4>Estatein Real Estate</h4>
+               <p>Web Development.</p>
+            </div>
          </div>
       </section>
    )
