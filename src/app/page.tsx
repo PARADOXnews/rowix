2857b66b30c457cb62ff01a40069b6ec54d54ef7
PARADOXnews <achirubashvili@gmail.com>
2025-12-01 23:07:21 +0400

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 // import Image from "next/image";
 import Header from "@/components/Header/Header";
 import styles from "./page.module.scss";
+import Solutions from "@/components/Solutions/Solutions";
 
 export default function Home() {
 
@@ -8,6 +9,8 @@ export default function Home() {
   return (
     <>
       <Header></Header>
+
+      <Solutions></Solutions>
     </>
   );
 }
--- src/components/Solutions/Solutions.tsx
+++ src/components/Solutions/Solutions.tsx
@@ -1,13 +1,17 @@
 import styles from "./Solutions.module.scss";
 
-export default function Solutions(){
+export default function Solutions() {
 
 
 
-   return(
+   return (
 
       <section className={styles.container}>
-          
+
+         <div>
+            <h4 className={styles.h4}>Digital Solutions</h4>
+            <h4 className={styles.h4}>That Drive Success</h4>
+         </div>
       </section>
    )
 }
