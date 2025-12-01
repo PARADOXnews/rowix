79d4d88fcfd9d122072cb5cca53fe8572bad34a4
damdebeli1 <notfunny77777@gmail.com>
2025-12-01 22:26:07 +0400

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,7 +6,18 @@ export default function Header() {
    return (
       <header className={styles.header}>
          header
-         {/* <Image src="/Header/1.png" width={200} height={300} alt="foto" /> */}
+           
+           <div className="bg-dark rounded padding flex">
+  ...content goes here...
+</div>
+
+
+<div className="w-12 h-12 bg-slightly-lighter rounded-lg overflow-hidden">
+  <img src="Logo.png" />
+</div>
+
+
+         
       </header>
    )
 }
