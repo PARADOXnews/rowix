7f76656a3df8716ef9137297022a43cdadeef26d
PARADOXnews <achirubashvili@gmail.com>
2025-12-03 01:26:11 +0400

    a

--- src/components/Arrow/Arrow.tsx
+++ src/components/Arrow/Arrow.tsx
@@ -12,7 +12,7 @@ export default function ({subHeader}:Props) {
 
 
    return (
-      <div className={`ati aic`}>
+      <div className={`ati aic ` + styles.arrow}>
          <Image src="/Title/arrow.png" width={52} height={52} alt="logo" />
          <h3 className={styles.h3}>{subHeader}</h3>
       </div>
--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -13,6 +13,8 @@ export default function ({ title, subHeader }: Props) {
 
 
    return (
+
+      
       <div className={`jcb aic` + styles.title}>
          <h1 className={styles.h1}>{title}</h1>
          {subHeader &&
